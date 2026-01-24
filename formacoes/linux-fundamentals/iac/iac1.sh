f7cb865a20f6708c8a1a029fae0508bd426aba34
Anderson Carlos <96142983+AndersonCSM@users.noreply.github.com>
2026-01-24 15:56:59 -0300

    restore files

---
+++ formacoes/linux-fundamentals/iac/iac1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /home/publico
+mkdir /home/adm
+mkdir /home/ven
+mkdir /home/sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash  -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /home/adm
+chown root:GRP_VEN /home/ven
+chown root:GRP_SEC /home/sec
+
+chmod 770 /home/adm
+chmod 770 /home/ven
+chmod 770 /home/sec
+chmod 777 /home/publico
+
+echo "Fim..."
